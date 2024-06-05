daf547ddf22044d6fc669181b7f948c02bdf911f
Tsing12Travel <Tsing12Travel@163.com>
2024-06-05 21:16:29 +0800

    No3_LengthOfLongestSubstring

---
+++ src/main/java/top100/No3_LengthOfLongestSubstring.java
@@ -0,0 +1,40 @@
+package top100;
+
+import java.util.HashSet;
+
+/* 3.无重复字符的最长字串 */
+// 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+
+public class No3_LengthOfLongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+
+        if (len <= 1) {
+            return len;
+        }
+
+        int left = 0;
+        int right = 1;
+        int res = 0;
+        // 哈希集合，记录每个字符是否出现过
+        HashSet<Character> set = new HashSet<>();
+        set.add(s.charAt(left));
+
+        while (left < right) {
+            while (left < right && !set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                right++;
+            }
+            res = Math.max(res, right - left);
+            set.remove(s.charAt(left));
+            left++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+}
