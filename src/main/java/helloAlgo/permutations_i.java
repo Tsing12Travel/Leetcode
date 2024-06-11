dd696983bf8cb9e77c39dc11507628852df0b3d0
Tsing12Travel <Tsing12Travel@163.com>
2024-06-11 12:07:29 +0800

    permutations_i

---
+++ src/main/java/helloAlgo/permutations_i.java
@@ -0,0 +1,38 @@
+package helloAlgo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 输入一个整数数组，其中不包含重复元素，返回所有可能的排列。 */
+/* 回溯算法：全排列I */
+public class permutations_i {
+    public void backtrack(List<Integer> state, int[] choices, boolean[] selected, List<List<Integer>> res) {
+        // 当状态长度等于元素数量时，记录解
+        if (state.size() == choices.length) {
+            res.add(new ArrayList<Integer>(state));
+            return;
+        }
+        // 遍历所有选择
+        for (int i = 0; i < choices.length; i++) {
+            int choice = choices[i];
+            // 剪枝：不允许重复选择元素
+            if (!selected[i]) {
+                // 尝试：做出选择，更新状态
+                selected[i] = true;
+                state.add(choice);
+                // 进行下一轮选择
+                backtrack(state, choices, selected, res);
+                // 回退：撤销选择，恢复到之前的状态
+                selected[i] = false;
+                state.remove(state.size() - 1);
+            }
+        }
+    }
+
+    /* 全排列I */
+    List<List<Integer>> permutationsI(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        backtrack(new ArrayList<Integer>(), nums, new boolean[nums.length], res);
+        return res;
+    }
+}
