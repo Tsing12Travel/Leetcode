bdefaac51e5521a9d398cb187faba7bb1c88b670
Tsing12Travel <Tsing12Travel@163.com>
2024-06-12 10:45:46 +0800

    ClimbingStairsDFS

--- src/main/java/helloAlgo/ClimbingStairsDFS.java
+++ src/main/java/helloAlgo/ClimbingStairsDFS.java
@@ -1,16 +1,20 @@
 package helloAlgo;
 
+/* 爬楼梯：给定一个共有 n 阶的楼梯，你每步可以上 1 阶或者 2 阶，请问有多少种方案可以爬到楼顶？ */
+/* 搜索 */
 public class ClimbingStairsDFS {
     public int dfs(int i) {
+        // 已知 dp[1] 和 dp[2] ，返回之
         if (i == 1 || i == 2) {
             return i;
         }
 
+        // dp[i] = dp[i-1] + dp[i-2]
         int count = dfs(i - 1) + dfs(i - 2);
         return count;
     }
 
-
+    /* 爬楼梯：搜索 */
     public int climbStairsDFS(int n) {
         return dfs(n);
     }
