132c88114e1d0c6487a1af00d686f4549332c544
Tsing12Travel <Tsing12Travel@163.com>
2024-06-20 12:02:06 +0800

    No136_DeleteNode

--- src/main/java/leetBook/No136_DeleteNode.java
+++ src/main/java/leetBook/No136_DeleteNode.java
@@ -1,2 +1,21 @@
-package leetBook;public class No136_DeleteNode {
+package leetBook;
+
+public class No136_DeleteNode {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) {
+            return head.next;
+        }
+
+        ListNode curr = head;
+        while (curr.next != null && curr.next.val != val) {
+            curr = curr.next;
+        }
+
+        if (curr.next != null) {
+            curr.next = curr.next.next;
+        }
+
+        return head;
+    }
 }
