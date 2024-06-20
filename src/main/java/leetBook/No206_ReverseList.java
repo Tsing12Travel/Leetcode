325eaabc15c1b5f8c8b35f5fc4986bc090047af4
Tsing12Travel <Tsing12Travel@163.com>
2024-06-20 15:52:31 +0800

    No206_ReverseList

---
+++ src/main/java/leetBook/No206_ReverseList.java
@@ -0,0 +1,26 @@
+package leetBook;
+
+public class No206_ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
