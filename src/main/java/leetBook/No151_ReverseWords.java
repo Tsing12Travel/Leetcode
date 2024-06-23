f56d6efc3072d43564f0a06112d1a7cbb94dbe40
Tsing12Travel <Tsing12Travel@163.com>
2024-06-23 16:34:43 +0800

    No151_ReverseWords

--- src/main/java/leetBook/No151_ReverseWords.java
+++ src/main/java/leetBook/No151_ReverseWords.java
@@ -1,8 +1,6 @@
 package leetBook;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class No151_ReverseWords {
     public static String reverseWords(String s) {
@@ -14,8 +12,104 @@ public class No151_ReverseWords {
         return String.join(" ", wordList);
     }
 
+    public static String reverseWords2(String s) {
+        StringBuilder sb = trimSpaces(s);
+
+        // 翻转字符串
+        reverse(sb, 0, sb.length() - 1);
+
+        // 翻转每个单词
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+
+    public static StringBuilder trimSpaces(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+
+        // 将字符串间多余的空白字符去除
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (c != ' ') {
+                sb.append(c);
+            } else if (sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+
+            ++left;
+        }
+        return sb;
+    }
+
+    public static void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
+    }
+
+    public static void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+            // 循环至单词的末尾
+            while (end < n && sb.charAt(end) != ' ') {
+                ++end;
+            }
+            // 翻转单词
+            reverse(sb, start, end - 1);
+            // 更新start，去找下一个单词
+            start = end + 1;
+            ++end;
+        }
+    }
+
+    public static String reverseWords3(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+
+        Deque<String> d = new ArrayDeque<String>();
+        StringBuilder word = new StringBuilder();
+
+        while (left <= right) {
+            char c = s.charAt(left);
+            if ((!word.isEmpty()) && (c == ' ')) {
+                // 将单词 push 到队列的头部
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+
+        return String.join(" ", d);
+    }
+
     public static void main(String[] args) {
         String str = "test a is This World! Hello";
-        System.out.println(reverseWords(str));
+        System.out.println(reverseWords3(str));
     }
 }
