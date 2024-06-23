f09960047630ac72633c03687b32c2f80bef8ed6
Tsing12Travel <Tsing12Travel@163.com>
2024-06-23 15:39:07 +0800

    No151_ReverseWords

--- src/main/java/leetBook/No151_ReverseWords.java
+++ src/main/java/leetBook/No151_ReverseWords.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 public class No151_ReverseWords {
     public static String reverseWords(String s) {
+        // 除去开头和末尾的空白字符
         s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
         List<String> wordList = Arrays.asList(s.split("\\s+"));
         Collections.reverse(wordList);
         return String.join(" ", wordList);
