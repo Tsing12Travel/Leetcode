a76b7f6bda44a38491c5b7bc3ba3e6dbfcda5c4c
Tsing12Travel <Tsing12Travel@163.com>
2024-06-24 18:33:34 +0800

    No53_MaxSubArray

--- src/main/java/top100/No53_MaxSubArray.java
+++ src/main/java/top100/No53_MaxSubArray.java
@@ -6,6 +6,7 @@ public class No53_MaxSubArray {
         int max = nums[0];
 
         for (int num : nums) {
+            // 如果前边累加后还不如自己本身大，那就把前边的都扔掉，从此自己本身重新开始累加
             pre = Math.max(pre + num, num);
             max = Math.max(max, pre);
         }
