c4d45bc3f722c2edc072a9a923fe64fb2200cc35
Tsing12Travel <Tsing12Travel@163.com>
2024-06-25 18:02:35 +0800

    No238_ProductExceptSelf

--- src/main/java/top100/No238_ProductExceptSelf.java
+++ src/main/java/top100/No238_ProductExceptSelf.java
@@ -29,4 +29,28 @@ public class No238_ProductExceptSelf {
 
         return res;
     }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+
+        // ans[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 0 的元素左侧没有元素， 所以 ans[0] = 1
+        ans[0] = 1;
+        for (int i = 1; i < len; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 ans[i]，右边的乘积为 R
+            ans[i] = ans[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+
+        return ans;
+    }
 }
