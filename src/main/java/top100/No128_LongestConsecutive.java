33b891651634db484a8b40b533f7b1f96ba8c892
Tsing12Travel <Tsing12Travel@163.com>
2024-06-27 21:37:17 +0800

    No128_LongestConsecutive

--- src/main/java/top100/No128_LongestConsecutive.java
+++ src/main/java/top100/No128_LongestConsecutive.java
@@ -4,6 +4,7 @@ package top100;
 // 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
 // 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -34,6 +35,28 @@ public class No128_LongestConsecutive {
         }
 
         return longestStreak;
+    }
+
+    public static int longestConsecutive2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int tempLength = 1;
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            } else if (nums[i] == nums[i - 1] + 1) {
+                tempLength++;
+            } else {
+                tempLength = 1;
+            }
+            res = Math.max(res, tempLength);
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
