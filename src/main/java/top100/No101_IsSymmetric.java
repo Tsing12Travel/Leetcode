af5ef2113967ddf33ddc3fca58e43ddda3576dba
Tsing12Travel <Tsing12Travel@163.com>
2024-06-28 19:16:14 +0800

    No101_IsSymmetric

--- src/main/java/top100/No101_IsSymmetric.java
+++ src/main/java/top100/No101_IsSymmetric.java
@@ -1,5 +1,8 @@
 package top100;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No101_IsSymmetric {
     public boolean isSymmetric(TreeNode root) {
         return root == null || recur(root.left, root.right);
@@ -11,6 +14,8 @@ public class No101_IsSymmetric {
         return recur(left.left, right.right) && recur(left.right, right.left);
     }
 
+    // 可以实现这样一个递归函数，通过「同步移动」两个指针的方法来遍历这棵树，p 指针和 q 指针一开始都指向这棵树的根，随后 p 右移时，q 左移，p 左移时，q 右移。
+    // 每次检查当前 p 和 q 节点的值是否相等，如果相等再判断左右子树是否对称
     public boolean isSymmetric2(TreeNode root) {
         return check(root, root);
     }
@@ -19,5 +24,32 @@ public class No101_IsSymmetric {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
         return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    public boolean isSymmetric3(TreeNode root) {
+        return check2(root, root);
+    }
+
+    public boolean check2(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
     }
 }
