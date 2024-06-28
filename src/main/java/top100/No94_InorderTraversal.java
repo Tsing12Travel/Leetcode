17f5a7552e455c3eefa3a5d423c77d8b90cae1fd
Tsing12Travel <Tsing12Travel@163.com>
2024-06-28 17:51:26 +0800

    No94_InorderTraversal

--- src/main/java/top100/No94_InorderTraversal.java
+++ src/main/java/top100/No94_InorderTraversal.java
@@ -38,6 +38,35 @@ public class No94_InorderTraversal {
         return res;
     }
 
+    public List<Integer> inorderTraversal3(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        while (root != null) {
+            //没有左子树，直接访问该节点，再访问右子树
+            if (root.left == null) {
+                ans.add(root.val);
+                root = root.right;
+            } else {
+                //有左子树，找前驱节点，判断是第一次访问还是第二次访问
+                TreeNode pre = root.left;
+                while (pre.right != null && pre.right != root)
+                    pre = pre.right;
+                //是第一次访问，访问左子树
+                if (pre.right == null) {
+                    pre.right = root;
+                    root = root.left;
+                }
+                //第二次访问了，那么应当消除链接
+                //该节点访问完了，接下来应该访问其右子树
+                else {
+                    pre.right = null;
+                    ans.add(root.val);
+                    root = root.right;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
