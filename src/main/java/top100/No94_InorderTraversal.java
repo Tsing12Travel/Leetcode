b1d308941f09d3b278686ab83c385e40875efb42
Tsing12Travel <Tsing12Travel@163.com>
2024-06-28 17:31:11 +0800

    No94_InorderTraversal

--- src/main/java/top100/No94_InorderTraversal.java
+++ src/main/java/top100/No94_InorderTraversal.java
@@ -1,21 +1,41 @@
 package top100;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class No94_InorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
-        inorder(root, res);
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
         return res;
     }
 
-    private void inorder(TreeNode root, List<Integer> res) {
+    private void dfs(TreeNode root, List<Integer> res) {
         if (root == null) return;
 
-        inorder(root.left, res);
+        dfs(root.left, res);
         res.add(root.val);
-        inorder(root.right, res);
+        dfs(root.right, res);
+    }
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
@@ -23,6 +43,6 @@ public class No94_InorderTraversal {
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
         No94_InorderTraversal solution = new No94_InorderTraversal();
-        System.out.println(solution.inorderTraversal(root));
+        System.out.println(solution.inorderTraversal2(root));
     }
 }
