afe5ddbb721ba09ea6dce1ee381e22d7f7276aae
Tsing12Travel <Tsing12Travel@163.com>
2024-06-28 16:45:50 +0800

    No94_InorderTraversal

--- src/main/java/top100/No94_InorderTraversal.java
+++ src/main/java/top100/No94_InorderTraversal.java
@@ -1,2 +1,28 @@
-package top100;public class No94_InorderTraversal {
+package top100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No94_InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        No94_InorderTraversal solution = new No94_InorderTraversal();
+        System.out.println(solution.inorderTraversal(root));
+    }
 }
