621447b54d7b03ece988b6832d241a354a3356ed
Tsing12Travel <Tsing12Travel@163.com>
2024-06-30 16:36:00 +0800

    No21_MergeTwoLists

---
+++ src/main/java/top100/No21_MergeTwoLists.java
@@ -0,0 +1,45 @@
+package top100;
+
+public class No21_MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = list1 == null ? list2 : list1;
+        /*if (list1 == null) {
+            curr.next = list2;
+        } else {
+            curr.next = list1;
+        }*/
+
+        return dummy.next;
+    }
+
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
