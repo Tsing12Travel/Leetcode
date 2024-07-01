6889c877d75e4f7fea7107046b2a9250a4ed917e
Tsing12Travel <Tsing12Travel@163.com>
2024-07-01 16:39:23 +0800

    No138_CopyRandomList

--- src/main/java/top100/No138_CopyRandomList.java
+++ src/main/java/top100/No138_CopyRandomList.java
@@ -40,4 +40,41 @@ public class No138_CopyRandomList {
         // 返回新链表的头节点
         return map.get(head);
     }
+
+
+    public Node copyRandomList2(Node head) {
+        if (head == null) return null;
+
+        Node curr = head;
+        // 复制各节点并构建拼接链表
+        while (curr != null) {
+            Node newNode = new Node(curr.val);
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next;
+        }
+
+        curr = head;
+        // 构建各新节点的 random 指向
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        // 拆分链表
+        curr = head.next;
+        Node prev = head;
+        Node res = head.next;
+        while (curr.next != null) {
+            curr.next = curr.next.next;
+            prev.next = prev.next.next;
+            prev = prev.next;
+            curr = curr.next;
+        }
+
+        prev.next = null;  // 原链表尾节点
+        return res;  // 返回新链表头节点
+    }
 }
