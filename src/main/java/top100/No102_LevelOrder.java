45587e71fb3c39624026c6d64d24912e818a5123
Tsing12Travel <Tsing12Travel@163.com>
2024-07-03 16:41:24 +0800

    No102_LevelOrder

--- src/main/java/top100/No102_LevelOrder.java
+++ src/main/java/top100/No102_LevelOrder.java
@@ -7,22 +7,40 @@ import java.util.Queue;
 
 public class No102_LevelOrder {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
 
         if (root != null) queue.add(root);
         while (!queue.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> level = new ArrayList<>();  // 将一层的数存入
+            int size = queue.size();  // 每层数字个数
 
-            for (int i = queue.size(); i > 0; i--) {
+            for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                temp.add(node.val);
+                level.add(node.val);
                 if (node.left != null) queue.add(node.left);
                 if (node.right != null) queue.add(node.right);
             }
-            res.add(temp);
+            res.add(level);
         }
 
         return res;
+    }
+
+
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+
+    public void dfs(TreeNode root, int level) {
+        if (root == null) return;
+        if (res.size() == level) res.add(new ArrayList<>());
+
+        res.get(level).add(root.val);
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
     }
 }
