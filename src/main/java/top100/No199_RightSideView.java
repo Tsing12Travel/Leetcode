32f41c9c768fe36fda7fe85b252020b1f114f48b
Tsing12Travel <Tsing12Travel@163.com>
2024-07-04 18:04:39 +0800

    No199_RightSideView

--- src/main/java/top100/No199_RightSideView.java
+++ src/main/java/top100/No199_RightSideView.java
@@ -1,12 +1,11 @@
 package top100;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class No199_RightSideView {
-
-
-
     // 深度优先
     private List<Integer> res = new ArrayList<>();
 
@@ -21,5 +20,44 @@ public class No199_RightSideView {
 
         dfs(root.right, depth + 1);
         dfs(root.left, depth + 1);
+    }
+
+
+    // 深度优先
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs2(root, 0, res);
+        return res;
+    }
+
+    private void dfs2(TreeNode root, int depth, List<Integer> res) {
+        if (root == null) return;
+        if (depth == res.size()) res.add(root.val);  // 这个深度首次遇到
+
+        dfs2(root.right, depth + 1, res);  // 先递归右子树，保证首次遇到的一定是最右边的节点
+        dfs2(root.left, depth + 1, res);
+    }
+
+
+    // 广度优先
+    public List<Integer> rightSideView3(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+                if (i == size - 1) res.add(node.val);  // i == size - 1 表示取每层的最后一个值
+            }
+        }
+
+        return res;
     }
 }
