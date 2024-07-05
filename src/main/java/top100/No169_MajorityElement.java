00c94a9822ddbc662e531b1f804c8a0f1344726c
Tsing12Travel <Tsing12Travel@163.com>
2024-07-05 17:51:34 +0800

    No169_MajorityElement

--- src/main/java/top100/No169_MajorityElement.java
+++ src/main/java/top100/No169_MajorityElement.java
@@ -27,4 +27,20 @@ public class No169_MajorityElement {
         int mid = nums.length / 2;
         return nums[mid];
     }
+
+
+    public int majorityElement3(int[] nums) {  // 需要考虑不存在众数的写法
+        int x = 0, votes = 0, count = 0;
+
+        for (int num : nums) {
+            if (votes == 0) x = num;
+            votes += num == x ? 1 : -1;
+        }
+
+        // 验证 x 是否为众数
+        for (int num : nums)
+            if (num == x) count++;
+
+        return count > nums.length / 2 ? x : 0;  // 当无众数时返回 0
+    }
 }
