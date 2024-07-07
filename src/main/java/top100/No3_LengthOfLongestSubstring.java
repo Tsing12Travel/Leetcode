302cd79b03a3a5af326c21d489624e01e6fa4e6c
Tsing12Travel <Tsing12Travel@163.com>
2024-07-07 17:09:25 +0800

    No3_LengthOfLongestSubstring

--- src/main/java/top100/No3_LengthOfLongestSubstring.java
+++ src/main/java/top100/No3_LengthOfLongestSubstring.java
@@ -1,5 +1,6 @@
 package top100;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /* 3.无重复字符的最长字串 */
@@ -33,8 +34,30 @@ public class No3_LengthOfLongestSubstring {
         return res;
     }
 
+
+    public static int lengthOfLongestSubstring2(String s) {
+        int len = s.length();
+        if (len == 0) return 0;
+
+        int left = 0;
+        int res = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < len; right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)));
+            }
+
+            map.put(s.charAt(right), right);
+            res = Math.max(res, right - left);
+        }
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
         String str = "pwwkew";
-        System.out.println(lengthOfLongestSubstring(str));
+        System.out.println(lengthOfLongestSubstring2(str));
     }
 }
