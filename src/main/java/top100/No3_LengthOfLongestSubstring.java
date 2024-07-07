363ea92c57be73b0478f0b6608ca3e1ae18b20d9
Tsing12Travel <Tsing12Travel@163.com>
2024-07-07 17:15:23 +0800

    No3_LengthOfLongestSubstring

--- src/main/java/top100/No3_LengthOfLongestSubstring.java
+++ src/main/java/top100/No3_LengthOfLongestSubstring.java
@@ -35,6 +35,11 @@ public class No3_LengthOfLongestSubstring {
     }
 
 
+    /* 步骤
+    * 哈希表 map 统计： 指针 j 遍历字符 s ，哈希表统计字符 s[j] 最后一次出现的索引
+    * 更新左指针 i ： 根据上轮左指针 i 和 map[s[j]] ，每轮更新左边界 i ，保证区间 [i+1,j] 内无重复字符且最大
+    * 更新结果 res ： 取上轮 res 和本轮双指针区间 [i+1,j] 的宽度（即 j−i ）中的最大值
+    */
     public static int lengthOfLongestSubstring2(String s) {
         int len = s.length();
         if (len == 0) return 0;
