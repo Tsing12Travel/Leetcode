684453106937a6f0351acd1d735d89e968875e87
Tsing12Travel <Tsing12Travel@163.com>
2024-07-08 19:09:47 +0800

    No239_MaxSlidingWindow

--- src/main/java/top100/No239_MaxSlidingWindow.java
+++ src/main/java/top100/No239_MaxSlidingWindow.java
@@ -1,9 +1,42 @@
 package top100;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class No239_MaxSlidingWindow {
-    public int[] maxSlidingWindow(int[] nums, int k) {}
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 0 || k == 0) return new int[0];
+
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[len - k + 1];
+
+        // 未形成窗口
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+
+        res[0] = deque.peekFirst();
+
+        // 形成窗口后
+        for (int i = k; i < len; i++) {
+            if (deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+
+        return res;
+    }
 
 
     // 暴力解法
@@ -29,7 +62,7 @@ public class No239_MaxSlidingWindow {
         int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
         int k = 3;
         No239_MaxSlidingWindow slidingWindow = new No239_MaxSlidingWindow();
-        int[] res = slidingWindow.maxSlidingWindow2(nums, k);
+        int[] res = slidingWindow.maxSlidingWindow(nums, k);
         System.out.println(Arrays.toString(res));
     }
 }
