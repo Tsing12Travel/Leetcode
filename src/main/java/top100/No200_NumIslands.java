abe8d1b69ff8a02ab0a3a4c6b6e73f363c147485
Tsing12Travel <Tsing12Travel@163.com>
2024-07-10 19:32:36 +0800

    No200_NumIslands

--- src/main/java/top100/No200_NumIslands.java
+++ src/main/java/top100/No200_NumIslands.java
@@ -24,4 +24,42 @@ public class No200_NumIslands {
         dfs(grid, i, j - 1);
         dfs(grid, i, j + 1);
     }
+
+
+    public int numIslands2(char[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs2(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs2(char[][] grid, int r, int c) {
+        // base case
+        if (!inArea(grid, r, c)) return;
+
+        // 如果这个格子不是岛屿，直接返回
+        if (grid[r][c] != '1') return;
+
+        // 将格子标记为“已遍历过”
+        grid[r][c] = '2';
+
+        // 访问上下左右四个相邻结点
+        dfs2(grid, r - 1, c);
+        dfs2(grid, r + 1, c);
+        dfs2(grid, r, c - 1);
+        dfs2(grid, r, c + 1);
+    }
+
+    // 判断坐标 (r, c) 是否在网格中
+    private boolean inArea(char[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
+    }
 }
