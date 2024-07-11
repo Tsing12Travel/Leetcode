6927db90a132fb266a2d01b670c98732cb7e19dc
Tsing12Travel <Tsing12Travel@163.com>
2024-07-11 16:48:58 +0800

    No74_SearchMatrix

---
+++ src/main/java/top100/No74_SearchMatrix.java
@@ -0,0 +1,38 @@
+package top100;
+
+public class No74_SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // 搜索的元素转化到矩阵中的坐标为 (mid / n, mid % n)
+            int x = matrix[mid / n][mid % n];
+
+            if (x == target) {
+                return true;
+            } else if (x < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+
+    // 暴力解法
+    public boolean searchMatrix3(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target) return true;
+            }
+        }
+
+        return false;
+    }
+}
