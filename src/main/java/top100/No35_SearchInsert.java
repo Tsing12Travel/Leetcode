8564c3f7c08be976a7be24138105a376bacf9719
Tsing12Travel <Tsing12Travel@163.com>
2024-07-11 16:33:12 +0800

    No35_SearchInsert

---
+++ src/main/java/top100/No35_SearchInsert.java
@@ -0,0 +1,21 @@
+package top100;
+
+public class No35_SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
