1e0da2397d818d7f9e6d78fc218cd62a076cd582
Tsing12Travel <Tsing12Travel@163.com>
2024-07-12 19:22:12 +0800

    No33_Search

--- src/main/java/top100/No33_Search.java
+++ src/main/java/top100/No33_Search.java
@@ -14,17 +14,17 @@ public class No33_Search {
             if (nums[mid] == target) return mid;
 
             // 将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的
-            if (nums[0] <= nums[mid]) {  // 左侧有序
-                if (nums[0] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {  // 右侧有序
+            if (nums[mid] <= nums[len - 1]) {  // 右侧有序
                 if (nums[mid] < target && target <= nums[len - 1]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
+                }
+            } else  {  // 左侧有序
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
         }
@@ -34,8 +34,8 @@ public class No33_Search {
 
     public static void main(String[] args) {
         No33_Search solution = new No33_Search();
-//        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
-        int[] nums = new int[]{5, 1, 3};
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+//        int[] nums = new int[]{5, 1, 3};
         System.out.println(solution.search(nums, 3));
     }
 }
