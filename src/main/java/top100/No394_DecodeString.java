f98149580e38ad0cfd219152665f068c94f6a795
Tsing12Travel <Tsing12Travel@163.com>
2024-07-14 16:42:47 +0800

    No394_DecodeString

--- src/main/java/top100/No394_DecodeString.java
+++ src/main/java/top100/No394_DecodeString.java
@@ -3,11 +3,12 @@ package top100;
 import java.util.Stack;
 
 public class No394_DecodeString {
+    // 辅助栈法
     public String decodeString(String s) {
         int multi = 0;
         StringBuilder res = new StringBuilder();
-        Stack<Integer> stack_multi = new Stack<>();
-        Stack<StringBuilder> stack_res = new Stack<>();
+        Stack<Integer> stack_multi = new Stack<>();  // 存储数字
+        Stack<StringBuilder> stack_res = new Stack<>();  // 存储字符串
 
         for (char c : s.toCharArray()) {
             if (c == '[') {
@@ -34,6 +35,36 @@ public class No394_DecodeString {
             }
         }
         return res.toString();
+    }
+
+
+    // 递归
+    public String decodeString2(String s) {
+        return dfs(s, 0)[0];
+    }
+
+    private String[] dfs(String s, int i) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+
+        while (i < s.length()) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9')
+                multi = multi * 10 + Integer.parseInt(String.valueOf(s.charAt(i)));
+            else if (s.charAt(i) == '[') {
+                String[] tmp = dfs(s, i + 1);
+                i = Integer.parseInt(tmp[0]);
+                while (multi > 0) {
+                    res.append(tmp[1]);
+                    multi--;
+                }
+            } else if (s.charAt(i) == ']')
+                return new String[]{String.valueOf(i), res.toString()};
+            else
+                res.append(String.valueOf(s.charAt(i)));
+            i++;
+        }
+
+        return new String[]{res.toString()};
     }
 
 
