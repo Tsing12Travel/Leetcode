80a7ad3b24b92f402fd368816f01eb7c5eba62a9
Tsing12Travel <Tsing12Travel@163.com>
2024-07-16 18:40:34 +0800

    No45_Jump

--- src/main/java/top100/No45_Jump.java
+++ src/main/java/top100/No45_Jump.java
@@ -1,6 +1,7 @@
 package top100;
 
 public class No45_Jump {
+    // 正向查找可到达的最大位置
     // 如果我们「贪心」地进行正向查找，每次找到可到达的最远位置，就可以在线性时间内得到最少的跳跃次数
     public int jump(int[] nums) {
         if (nums.length == 0) return 0;
@@ -19,6 +20,28 @@ public class No45_Jump {
             }
         }
         return step;
+    }
+
+
+    // 反向查找出发位置
+    /*我们的目标是到达数组的最后一个位置，因此我们可以考虑最后一步跳跃前所在的位置，该位置通过跳跃能够到达最后一个位置。
+    如果有多个位置通过跳跃都能够到达最后一个位置，那么我们应该如何进行选择呢？
+    直观上来看，我们可以「贪心」地选择距离最后一个位置最远的那个位置，也就是对应下标最小的那个位置。
+    因此，我们可以从左到右遍历数组，选择第一个满足要求的位置。
+    找到最后一步跳跃前所在的位置之后，我们继续贪心地寻找倒数第二步跳跃前所在的位置，以此类推，直到找到数组的开始位置。*/
+    public int jump2(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
     }
 
 
