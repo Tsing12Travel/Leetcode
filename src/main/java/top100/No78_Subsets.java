d35728a87d57a19c5de4000b3dc7be4360d12e52
Tsing12Travel <Tsing12Travel@163.com>
2024-07-18 20:28:53 +0800

    No78_Subsets

--- src/main/java/top100/No78_Subsets.java
+++ src/main/java/top100/No78_Subsets.java
@@ -5,6 +5,27 @@ import java.util.List;
 
 public class No78_Subsets {
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (len == 0) return res;
+
+        List<Integer> path = new ArrayList<>();
+        backtrack(nums, 0, path, res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int depth, List<Integer> path, List<List<Integer>> res) {
+        res.add(new ArrayList<>(path));  // 复制 path
+
+        for (int i = depth; i < nums.length; i++) {  // 枚举选择的数字
+            path.add(nums[i]);
+            backtrack(nums, i + 1, path, res);
+            path.removeLast();  // 恢复现场
+        }
+    }
+
+
+    public List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         int len = nums.length;
         if (len == 0) return res;
