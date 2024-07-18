0b070b9731ca40a01cd633491f4f55d5cce622cc
Tsing12Travel <Tsing12Travel@163.com>
2024-07-18 17:23:57 +0800

    No46_Permute

--- src/main/java/top100/No46_Permute.java
+++ src/main/java/top100/No46_Permute.java
@@ -6,6 +6,33 @@ import java.util.List;
 public class No46_Permute {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+
+        boolean[] on_path = new boolean[nums.length];
+        List<Integer> path = new ArrayList<>();
+        backtrack(nums, path, on_path, res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, List<Integer> path, boolean[] on_path, List<List<Integer>> res) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (on_path[i]) continue;
+            path.add(nums[i]);
+            on_path[i] = true;
+            backtrack(nums, path, on_path, res);
+            on_path[i] = false;
+            path.removeLast();
+        }
+    }
+
+
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         int len = nums.length;
         if (len == 0) return res;
 
