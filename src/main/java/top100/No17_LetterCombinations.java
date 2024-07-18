56d5b8d804a4148440873418ed0a6e910e05f25b
Tsing12Travel <Tsing12Travel@163.com>
2024-07-18 21:45:36 +0800

    No17_LetterCombinations

--- src/main/java/top100/No17_LetterCombinations.java
+++ src/main/java/top100/No17_LetterCombinations.java
@@ -30,8 +30,64 @@ public class No17_LetterCombinations {
     }
 
 
+    public List<String> letterCombinations2(String digits) {
+        List<String> res = new ArrayList<>();
+        int len = digits.length();
+        if (len == 0) return res;
+
+        StringBuilder path = new StringBuilder();
+        backtrack(digits, 0, path, res);
+        return res;
+    }
+
+    private void backtrack(String digits, int depth, StringBuilder path, List<String> res) {
+        if (depth == digits.length()) {
+            res.add(path.toString());
+            return;
+        }
+
+        char x = digits.charAt(depth);
+        String str = MAPPING[x - '0'];
+        for (int i = 0; i < str.length(); i++) {
+            path.append(str.charAt(i));
+            backtrack(digits, i + 1, path, res);
+            path.deleteCharAt(path.length() - 1);
+        }
+        /*// 也可写成如下循环
+        for (char c : MAPPING[digits.charAt(depth) - '0'].toCharArray()) {
+            path.append(c);
+            backtrack(digits, depth + 1, path, res);
+            path.deleteCharAt(path.length() - 1);
+        }*/
+    }
+
+
+    // backtrack 不传递 depth 的版本(会比较抽象)
+    public List<String> letterCombinations3(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits.isEmpty()) return res;
+
+        StringBuilder path = new StringBuilder();
+        backtrack2(digits, path, res);
+        return res;
+    }
+
+    private void backtrack2(String digits, StringBuilder path, List<String> res) {
+        if (path.length() == digits.length()) {
+            res.add(path.toString());
+            return;
+        }
+
+        for (char c : MAPPING[digits.charAt(path.length()) - '0'].toCharArray()) {
+            path.append(c);
+            backtrack2(digits, path, res);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        String digits = "23";
+         String digits = "23";
+//        String digits = "2";
         No17_LetterCombinations no17 = new No17_LetterCombinations();
         List<String> res = no17.letterCombinations(digits);
         System.out.println(res);
