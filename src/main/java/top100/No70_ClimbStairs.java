a87d4e122b5794b18204eee64096c4f139c53436
Tsing12Travel <Tsing12Travel@163.com>
2024-07-20 17:58:26 +0800

    No70_ClimbStairs

--- src/main/java/top100/No70_ClimbStairs.java
+++ src/main/java/top100/No70_ClimbStairs.java
@@ -30,9 +30,40 @@ public class No70_ClimbStairs {
     }
 
 
+    // 1:1 翻译成递推
+    public int climbStairs3(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+    // 空间优化
+    /* 观察状态转移方程，发现一旦算出 f[i]，那么 f[i−2] 及其左边的状态就永远不会用到了。
+    这意味着每次循环，只需要知道「上一个状态」和「上上一个状态」的 f 值是多少，分别记作 f0、f1，它俩的初始值均为 1，对应着 f[1] 和 f[0]
+    每次循环计算出新的状态，即 f = f0 + f1，那么对下一轮循环来说有：
+    「上上一个状态」就是 f1，更新 f0 = f1
+    「上一个状态」就是 f，更新 f1 = f
+    最后答案为 f1，因为最后一轮循环算出的 f 赋给了 f1 */
+    public int climbStairs4(int n) {
+        int f0 = 1, f1 = 1;
+        for (int i = 2; i <= n; i++) {
+            int f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        return f1;
+    }
+
+
     public static void main(String[] args) {
         int n = 45;
         No70_ClimbStairs solution = new No70_ClimbStairs();
-        System.out.println(solution.climbStairs2(n));
+        System.out.println(solution.climbStairs4(n));
     }
 }
