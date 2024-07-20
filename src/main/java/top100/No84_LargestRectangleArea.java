e03b17620c8b5ca1ba82ae4e1ba225809ecb6b8b
Tsing12Travel <Tsing12Travel@163.com>
2024-07-20 15:59:11 +0800

    No84_LargestRectangleArea

--- src/main/java/top100/No84_LargestRectangleArea.java
+++ src/main/java/top100/No84_LargestRectangleArea.java
@@ -48,10 +48,67 @@ public class No84_LargestRectangleArea {
     }
 
 
+    // 暴力解法：枚举以每个柱形为高度的最大矩形的面积。即依次遍历柱形的高度，对于每一个高度分别向两边扩散，求出以当前高度为矩形的最大宽度多少
+    public int largestRectangleArea2(int[] heights) {
+        int len = heights.length;
+        if (len == 0) return 0;  // 特判
+        int max = 0;
+
+        for (int i = 0; i < len; i++) {
+            int currHeight = heights[i];
+
+            // 找左边最后 1 个大于等于 heights[i] 的下标
+            int left = i;
+            while (left > 0 && heights[left - 1] >= currHeight) {
+                left--;
+            }
+
+            // 找右边最后 1 个大于等于 heights[i] 的索引
+            int right = i;
+            while (right < len - 1 && heights[right + 1] >= currHeight) {
+                right++;
+            }
+
+            max = Math.max(max, (right - left + 1) * currHeight);
+        }
+        return max;
+    }
+
+
+    public int largestRectangleArea3(int[] heights) {
+        int len = heights.length;
+        if (len == 0) return 0;
+        if (len == 1) return heights[0];
+
+        int res = 0;
+
+        int[] newHeights = new int[len + 2];  // 首尾存储哨兵(均为 0)，故长度需要 +2
+        newHeights[0] = 0;
+        System.arraycopy(heights, 0, newHeights, 1, len);
+        newHeights[len + 1] = 0;
+        len += 2;  // 新数组的长度
+        heights = newHeights;  // newHeights 赋给 heights，竟然节省了内存？！
+
+        Deque<Integer> stack = new ArrayDeque<>(len);
+        // 先放入哨兵，在循环里就不用做非空判断
+        stack.addLast(0);
+
+        for (int i = 1; i < len; i++) {
+            while (heights[i] < heights[stack.peekLast()]) {
+                int curHeight = heights[stack.pollLast()];
+                int curWidth = i - stack.peekLast() - 1;
+                res = Math.max(res, curHeight * curWidth);
+            }
+            stack.addLast(i);
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         // int[] heights = new int[]{2, 1, 5, 6, 2, 3};
         int[] heights = new int[]{2, 4};
         No84_LargestRectangleArea test = new No84_LargestRectangleArea();
-        System.out.println(test.largestRectangleArea(heights));
+        System.out.println(test.largestRectangleArea3(heights));
     }
 }
