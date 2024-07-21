5b45989347118d2a3a3089d4fe3ff99d8cf2b2cb
Tsing12Travel <Tsing12Travel@163.com>
2024-07-21 19:39:30 +0800

    No322_CoinChange

--- src/main/java/top100/No322_CoinChange.java
+++ src/main/java/top100/No322_CoinChange.java
@@ -75,6 +75,21 @@ public class No322_CoinChange {
     }
 
 
+    // 空间优化：一个数组
+    public int coinChange4(int[] coins, int amount) {
+        int[] f = new int[amount + 1];
+        Arrays.fill(f, Integer.MAX_VALUE / 2);
+        f[0] = 0;
+        for (int x : coins) {
+            for (int c = x; c <= amount; c++) {
+                f[c] = Math.min(f[c], f[c - x] + 1);
+            }
+        }
+        int ans = f[amount];
+        return ans < Integer.MAX_VALUE / 2 ? ans : -1;
+    }
+
+
     public static void main(String[] args) {
         int[] coins = {1, 2, 5};
         int amount = 11;
