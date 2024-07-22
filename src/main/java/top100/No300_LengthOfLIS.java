e359f8c39b9cd95eb64a89db7d549cb3eddb2368
Tsing12Travel <Tsing12Travel@163.com>
2024-07-22 15:43:31 +0800

    No300_LengthOfLIS

--- src/main/java/top100/No300_LengthOfLIS.java
+++ src/main/java/top100/No300_LengthOfLIS.java
@@ -21,13 +21,35 @@ public class No300_LengthOfLIS {
     private int dfs(int i) {
         if (dp[i] != 0) return dp[i];  // 之前计算过
 
+        dp[i] = 1;  // 初始化 dp[i]，至少包含 nums[i] 本身
         for (int j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                dp[i] = Math.max(dp[i], dfs(j));
+                dp[i] = Math.max(dp[i], dfs(j) + 1);
             }
         }
 
-        return ++dp[i];
+        return dp[i];
+    }
+
+
+    // 递推。f[i] 表示以 nums[i] 结尾的最长递增子序列（LIS）的长度
+    public int lengthOfLIS2(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        int[] f = new int[len];
+        int res = 0;
+
+        for (int i = 0; i < len; i++) {
+            f[i] = 1;  // 初始时，以 nums[i] 结尾的 LIS 至少为 1
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            res = Math.max(res, f[i]);
+        }
+
+        return res;
     }
 
 
