1fb78a88b55101bf8edc29324f392105694fda5d
Tsing12Travel <Tsing12Travel@163.com>
2024-07-22 15:50:48 +0800

    No300_LengthOfLIS

--- src/main/java/top100/No300_LengthOfLIS.java
+++ src/main/java/top100/No300_LengthOfLIS.java
@@ -1,5 +1,8 @@
 package top100;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class No300_LengthOfLIS {
     private int[] nums;
     private int[] dp;
@@ -77,6 +80,38 @@ public class No300_LengthOfLIS {
         }
 
         return maxLIS;
+    }
+
+
+    // 贪心 + 二分查找。使用额外空间
+    public int lengthOfLIS4(int[] nums) {
+        List<Integer> g = new ArrayList<>();
+        for (int x : nums) {
+            int j = lowerBound(g, x);
+            if (j == g.size()) {
+                g.add(x);  // >=x 的 g[j] 不存在
+            } else {
+                g.set(j, x);
+            }
+        }
+        return g.size();
+    }
+
+    // 开区间写法
+    private int lowerBound(List<Integer> g, int target) {
+        int left = -1, right = g.size();  // 开区间 (left, right)
+        while (left + 1 < right) {  // 区间不为空
+            // 循环不变量：
+            // nums[left] < target
+            // nums[right] >= target
+            int mid = left + (right - left) / 2;
+            if (g.get(mid) < target) {
+                left = mid;  // 范围缩小到 (mid, right)
+            } else {
+                right = mid;  // 范围缩小到 (left, mid)
+            }
+        }
+        return right;  // 或者 left + 1
     }
 
 
