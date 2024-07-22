f4d78abce956f17ebf88edfc7dca862cb16d7d80
Tsing12Travel <Tsing12Travel@163.com>
2024-07-22 17:50:59 +0800

    No416_CanPartition

--- src/main/java/top100/No416_CanPartition.java
+++ src/main/java/top100/No416_CanPartition.java
@@ -3,6 +3,17 @@ package top100;
 import java.util.Arrays;
 
 public class No416_CanPartition {
+    // 记忆化搜索
+    // 定义 dfs(i, j) 表示能否从 nums[0] 到 nums[i] 中选出一个和恰好等于 j 的子序列
+    /* 考虑 nums[i] 选或不选：
+    选：问题变成能否从 nums[0] 到 nums[i − 1] 中选出一个和恰好等于 j − nums[i] 的子序列，即 dfs(i − 1, j − nums[i])。
+    不选：问题变成能否从 nums[0] 到 nums[i − 1] 中选出一个和恰好等于 j 的子序列，即 dfs(i − 1, j)。
+
+    这两个只要有一个成立，dfs(i, j) 就是 true，即 dfs(i, j) = dfs(i − 1, j − nums[i]) || dfs(i − 1, j)
+    代码实现时，可以只在 j ≥ nums[i] 时才调用 dfs(i − 1, j − nums[i])，因为任何子序列的和都不会是负的
+
+    递归边界：dfs(−1, 0) = true, dfs(−1, >0) = false
+    递归入口：dfs(n − 1, s/2)，即答案 */
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) {
@@ -30,6 +41,7 @@ public class No416_CanPartition {
 
         boolean res = left >= nums[i] && dfs(i - 1, left - nums[i], nums, memo) || dfs(i - 1, left, nums, memo);
         memo[i][left] = res ? 1 : 0;  // 记忆化
+
         return res;
     }
 }
