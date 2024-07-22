ce754d59301e0a7c7ef1a4b008e38f8fa5e6b929
Tsing12Travel <Tsing12Travel@163.com>
2024-07-22 18:00:19 +0800

    No416_CanPartition

--- src/main/java/top100/No416_CanPartition.java
+++ src/main/java/top100/No416_CanPartition.java
@@ -44,4 +44,45 @@ public class No416_CanPartition {
 
         return res;
     }
+
+
+    // 1:1 翻译成递推
+    // [i][j] 的定义和 dfs(i, j) 的定义是一样的，都表示能否从 nums[0] 到 nums[i] 中选出一个和恰好等于 j 的子序列
+    /* 相应的递推式（状态转移方程）也和 dfs 一样：f[i][j] = f[i − 1][j − nums[i]] || f[i − 1][j]
+    但是，这种定义方式没有状态能表示递归边界，即 i = −1 的情况
+    解决办法：在二维数组 f 的最上边插入一排状态，那么其余状态全部向下偏移一位，把 f[i] 改为 f[i + 1]，把 f[i − 1] 改为 f[i]
+    修改后 f[i + 1][j] 表示能否从 nums[0] 到 nums[i] 中选出一个和为 j 的子序列。f[0] 对应递归边界。
+    修改后的递推式为：f[i + 1][j] = f[i][j − nums[i]] || f[i][j]
+
+    初始值 f[0][0] = true，翻译自递归边界 dfs(−1, 0) = true。其余值初始化成 false
+    答案为 f[n][s/2]，翻译自递归入口 dfs(n − 1, s/2) */
+    public boolean canPartition2(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) return false;
+
+        sum /= 2;  // 注意这里把 s 减半了
+        int len = nums.length;
+        boolean[][] f = new boolean[len + 1][sum + 1];
+        f[0][0] = true;
+
+        for (int i = 0; i < len; i++) {
+            int x = nums[i];
+            for (int j = 0; j <= sum; j++) {
+                f[i + 1][j] = j >= x && f[i][j - x] || f[i][j];
+            }
+        }
+
+        return f[len][sum];
+    }
+
+
+    public static void main(String[] args) {
+        // int[] nums = new int[]{1, 5, 11, 5};
+        int[] nums = new int[]{2, 1, 1, 2};
+        No416_CanPartition sol = new No416_CanPartition();
+        System.out.println(sol.canPartition2(nums));
+    }
 }
