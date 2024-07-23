01640bb65de2e64792b02d8ae00140b578833efc
Tsing12Travel <Tsing12Travel@163.com>
2024-07-23 15:28:54 +0800

    No62_UniquePaths

--- src/main/java/top100/No62_UniquePaths.java
+++ src/main/java/top100/No62_UniquePaths.java
@@ -24,10 +24,39 @@ public class No62_UniquePaths {
     }
 
 
+    // 由于 f(i, j) 仅与第 i 行和第 i − 1 行的状态有关，因此我们可以使用滚动数组代替代码中的二维数组，使空间复杂度降低为 O(n)
+    public int uniquePaths2(int m, int n) {
+        int[] f = new int[n];
+        Arrays.fill(f, 1);  // 左上角 f[0] = 1(自己到自己有一种方法)，故到其他位置至少有一种方法，填充 1
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[j] += f[j - 1];
+            }
+        }
+
+        return f[n - 1];
+    }
+
+
+    // 组合数学
+    // 从左上角到右下角的过程中，我们需要移动 m + n − 2 次，其中有 m − 1 次向下移动，n − 1 次向右移动。
+    // 因此路径的总数，就等于从 m + n − 2 次移动中选择 m − 1 次向下移动的方案数 -> C(m − 1, m + n − 2)
+    public int uniquePaths3(int m, int n) {
+        long ans = 1;
+
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+
+        return (int) ans;
+    }
+
+
     public static void main(String[] args) {
         int m = 3;
         int n = 7;
         No62_UniquePaths sol = new No62_UniquePaths();
-        System.out.println(sol.uniquePaths(m, n));
+        System.out.println(sol.uniquePaths3(m, n));
     }
 }
