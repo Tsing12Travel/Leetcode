0f99c11ec2b0abfd199d35777ea5da626d74b7e1
Tsing12Travel <Tsing12Travel@163.com>
2024-07-23 17:06:38 +0800

    No64_MinPathSum

--- src/main/java/top100/No64_MinPathSum.java
+++ src/main/java/top100/No64_MinPathSum.java
@@ -10,11 +10,11 @@ public class No64_MinPathSum {
         int[][] dp = new int[row][col];
 
         // 边界条件：第一行、第一列
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < row; i++) {  //
+        dp[0][0] = grid[0][0];  // 左上角
+        for (int i = 1; i < row; i++) {  // 第一列
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
-        for (int j = 1; j < col; j++) {
+        for (int j = 1; j < col; j++) {  // 第一行
             dp[0][j] = dp[0][j - 1] + grid[0][j];
         }
 
@@ -28,9 +28,31 @@ public class No64_MinPathSum {
     }
 
 
+    // 可以复用 grid 矩阵
+    public int minPathSum2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (j == 0) {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                } else if (i == 0) {
+                    grid[i][j] = grid[i][j - 1] + grid[i][j];
+                } else {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+
+
     public static void main(String[] args) {
         int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         No64_MinPathSum sol = new No64_MinPathSum();
-        System.out.println(sol.minPathSum(grid));
+        System.out.println(sol.minPathSum2(grid));
     }
 }
