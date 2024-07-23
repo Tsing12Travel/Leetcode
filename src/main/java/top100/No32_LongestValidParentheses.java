caf3424ffb7f729c6073bbe2518d3031f8788585
Tsing12Travel <Tsing12Travel@163.com>
2024-07-23 11:15:15 +0800

    No32_LongestValidParentheses

--- src/main/java/top100/No32_LongestValidParentheses.java
+++ src/main/java/top100/No32_LongestValidParentheses.java
@@ -29,11 +29,52 @@ public class No32_LongestValidParentheses {
     }
 
 
+    public int longestValidParentheses2(String s) {
+        int len = s.length();
+        int left = 0, right = 0;
+        int res = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                res = Math.max(res, 2 * left);
+            } else if (left > right) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        left = 0;
+        right = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                res = Math.max(res, 2 * left);
+            } else if (left > right) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
         // String s = "(()";
-        // String s = ")()())";
-        String s = "";
+        String s = ")()())";
+        // String s = "";
         No32_LongestValidParentheses sol = new No32_LongestValidParentheses();
-        System.out.println(sol.longestValidParentheses(s));
+        System.out.println(sol.longestValidParentheses2(s));
     }
 }
