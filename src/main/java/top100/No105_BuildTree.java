8c5ca87ff9c2e1aa41f8ea212866b971fea4d4d6
Tsing12Travel <Tsing12Travel@163.com>
2024-07-24 18:42:32 +0800

    No105_BuildTree

--- src/main/java/top100/No105_BuildTree.java
+++ src/main/java/top100/No105_BuildTree.java
@@ -41,6 +41,32 @@ public class No105_BuildTree {
     }
 
 
+    int[] preorder;
+    HashMap<Integer, Integer> dic = new HashMap<>();
+
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        for (int i = 0; i < inorder.length; i++)
+            dic.put(inorder[i], i);
+        return recur(0, 0, inorder.length - 1);
+    }
+
+    TreeNode recur(int root, int left, int right) {
+        // 递归终止
+        if (left > right) return null;
+
+        // 建立根节点
+        TreeNode node = new TreeNode(preorder[root]);
+        // 划分根节点、左子树、右子树
+        int i = dic.get(preorder[root]);
+
+        node.left = recur(root + 1, left, i - 1);              // 开启左子树递归
+        // `root + i - left + 1` 含义为 `根节点索引 + 左子树长度 + 1`
+        node.right = recur(root + i - left + 1, i + 1, right);  // 开启右子树递归
+        return node;                                                      // 回溯返回根节点
+    }
+
+
     public static void main(String[] args) {
         int[] preorder = {3, 9, 20, 15, 7};
         int[] inorder = {9, 3, 15, 20, 7};
