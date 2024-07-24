3b176f75ff2cdee3cd9d5f6af3dc06ad0ae5f139
Tsing12Travel <Tsing12Travel@163.com>
2024-07-24 15:35:37 +0800

    No287_FindDuplicate

--- src/main/java/top100/No287_FindDuplicate.java
+++ src/main/java/top100/No287_FindDuplicate.java
@@ -21,6 +21,32 @@ public class No287_FindDuplicate {
     }
 
 
+    // 二分查找
+    public int findDuplicate2(int[] nums) {
+        int n = nums.length;
+        int l = 1, r = n - 1, ans = -1;
+
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int cnt = 0;
+
+            for (int num : nums) {
+                if (num <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         // int[] nums = {1, 3, 4, 2, 2};
         int[] nums = {3, 3, 3, 3, 3};
