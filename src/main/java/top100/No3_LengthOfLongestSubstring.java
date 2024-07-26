30af9f900d0b10e2bec8dcc23cba368ce0d85c20
Tsing12Travel <Tsing12Travel@163.com>
2024-07-26 17:09:34 +0800

    No3_LengthOfLongestSubstring

--- src/main/java/top100/No3_LengthOfLongestSubstring.java
+++ src/main/java/top100/No3_LengthOfLongestSubstring.java
@@ -37,10 +37,10 @@ public class No3_LengthOfLongestSubstring {
 
 
     /* 步骤
-    * 哈希表 map 统计：指针 j 遍历字符 s，哈希表统计字符 s[j] 最后一次出现的索引
-    * 更新左指针 i：根据上轮左指针 i 和 map[s[j]]，每轮更新左边界 i，保证区间 [i + 1, j] 内无重复字符且最大
-    * 更新结果 res：取上轮 res 和本轮双指针区间 [i + 1, j] 的宽度（即 j − i）中的最大值
-    */
+     * 哈希表 map 统计：指针 j 遍历字符 s，哈希表统计字符 s[j] 最后一次出现的索引
+     * 更新左指针 i：根据上轮左指针 i 和 map[s[j]]，每轮更新左边界 i，保证区间 [i + 1, j] 内无重复字符且最大
+     * 更新结果 res：取上轮 res 和本轮双指针区间 [i + 1, j] 的宽度（即 j − i）中的最大值
+     */
     public static int lengthOfLongestSubstring2(String s) {
         int len = s.length();
         if (len == 0) return 0;
@@ -67,18 +67,19 @@ public class No3_LengthOfLongestSubstring {
     public int lengthOfLongestSubstring3(String s) {
         Map<Character, Integer> dic = new HashMap<>();
         int res = 0, tmp = 0, len = s.length();
-        for(int j = 0; j < len; j++) {
-            int i = dic.getOrDefault(s.charAt(j), -1); // 获取索引 i
-            dic.put(s.charAt(j), j); // 更新哈希表
-            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
-            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        for (int j = 0; j < len; j++) {
+            int i = dic.getOrDefault(s.charAt(j), -1);  // 获取索引 i
+            dic.put(s.charAt(j), j);  // 更新哈希表
+            tmp = tmp < j - i ? tmp + 1 : j - i;  // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp);  // max(dp[j - 1], dp[j])
         }
         return res;
     }
 
 
     public static void main(String[] args) {
-        String str = "abba";
+        // String str = "abba";
+        String str = "pwwkew";
         System.out.println(lengthOfLongestSubstring2(str));
     }
 }
