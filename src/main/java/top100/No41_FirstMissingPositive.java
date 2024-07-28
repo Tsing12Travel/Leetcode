1a4be856199b01b35db2df2e5a6cb445d56b7003
Tsing12Travel <Tsing12Travel@163.com>
2024-07-28 21:50:49 +0800

    No41_FirstMissingPositive

--- src/main/java/top100/No41_FirstMissingPositive.java
+++ src/main/java/top100/No41_FirstMissingPositive.java
@@ -1,5 +1,7 @@
 package top100;
 
+import java.util.Arrays;
+
 public class No41_FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
         int len = nums.length;
@@ -8,6 +10,7 @@ public class No41_FirstMissingPositive {
             // 满足在指定范围内且没有放在正确的位置上，才进行交换
             // 如：数字 3 应该放在索引为 2 的位置上
             // 数值为 i 的数映射到下标为 i - 1 的位置。原地哈希就相当于，让每个数字 n 都回到下标为 n - 1 的家里
+            // while (nums[i] > 0 && nums[i] <= len && nums[i] - 1 != i) {  // 这个判断条件也正确
             while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
                 swap(nums, nums[i] - 1, i);
             }
@@ -27,5 +30,15 @@ public class No41_FirstMissingPositive {
         int temp = nums[index1];
         nums[index1] = nums[index2];
         nums[index2] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        // int[] nums = new int[]{3, 4, -1, 1};
+        // int[] nums = new int[]{7, 8, 9, 11, 12};
+        int[] nums = new int[]{7, 8, 9, 3, 12};
+        No41_FirstMissingPositive sol = new No41_FirstMissingPositive();
+        System.out.println(sol.firstMissingPositive(nums));
+        System.out.println(Arrays.toString(nums));
     }
 }
