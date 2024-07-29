b2c288a5a43472faf6c93b46f66df9c3221d401c
Tsing12Travel <Tsing12Travel@163.com>
2024-07-29 12:12:05 +0800

    No240_SearchMatrix

--- src/main/java/top100/No240_SearchMatrix.java
+++ src/main/java/top100/No240_SearchMatrix.java
@@ -14,7 +14,12 @@ public class No240_SearchMatrix {
 
 
     /* 将矩阵逆时针旋转 45° ，并将其转化为图形式，发现其类似于 二叉搜索树 ，即对于每个元素，其左分支元素更小、右分支元素更大。
-    因此，通过从 “根节点” 开始搜索，遇到比 target 大的元素就向左，反之向右，即可找到目标值 target */
+    因此，通过从 “根节点” 开始搜索，遇到比 target 大的元素就向左，反之向右，即可找到目标值 target
+
+    “根节点” 对应的是矩阵的 “左下角” 和 “右上角” 元素，暂称之为 标志数 ，以 matrix 中的 左下角元素 为标志数 flag ，则有:
+        若 flag > target ，则 target 一定在 flag 所在 行的上方 ，即 flag 所在行可被消去。
+        若 flag < target ，则 target 一定在 flag 所在 列的右方 ，即 flag 所在列可被消去。
+    */
     public boolean searchMatrix2(int[][] matrix, int target) {
         int i = matrix.length - 1, j = 0;
 
