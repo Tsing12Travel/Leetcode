2c73a168eb58218a852405b4a66a37e23e619019
Tsing12Travel <Tsing12Travel@163.com>
2024-07-29 12:10:48 +0800

    No240_SearchMatrix

--- src/main/java/top100/No240_SearchMatrix.java
+++ src/main/java/top100/No240_SearchMatrix.java
@@ -1,16 +1,20 @@
 package top100;
 
 public class No240_SearchMatrix {
+    // 暴力解法
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == target) return true;
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                if (anInt == target) return true;
             }
         }
 
         return false;
     }
 
+
+    /* 将矩阵逆时针旋转 45° ，并将其转化为图形式，发现其类似于 二叉搜索树 ，即对于每个元素，其左分支元素更小、右分支元素更大。
+    因此，通过从 “根节点” 开始搜索，遇到比 target 大的元素就向左，反之向右，即可找到目标值 target */
     public boolean searchMatrix2(int[][] matrix, int target) {
         int i = matrix.length - 1, j = 0;
 
@@ -26,6 +30,7 @@ public class No240_SearchMatrix {
 
         return false;
     }
+
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}};
