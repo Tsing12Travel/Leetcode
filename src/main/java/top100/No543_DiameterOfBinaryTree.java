e9425e7b24629469e6c8ff590fe20b40801e6c38
Tsing12Travel <Tsing12Travel@163.com>
2024-07-30 12:09:01 +0800

    No543_DiameterOfBinaryTree

--- src/main/java/top100/No543_DiameterOfBinaryTree.java
+++ src/main/java/top100/No543_DiameterOfBinaryTree.java
@@ -2,7 +2,6 @@ package top100;
 
 public class No543_DiameterOfBinaryTree {
     private int diameter = 0;
-
     public int diameterOfBinaryTree(TreeNode root) {
         dfs(root);
         return diameter;
@@ -21,7 +20,6 @@ public class No543_DiameterOfBinaryTree {
 
     // ans 表示以 node 为起点，经过该点的最长路径点数数值
     int ans = 1;
-
     public int diameterOfBinaryTree2(TreeNode root) {
         depth(root);
         return ans - 1;
@@ -39,7 +37,6 @@ public class No543_DiameterOfBinaryTree {
 
 
     int max;
-
     public int diameterOfBinaryTree3(TreeNode root) {
         max = 0;
         postTraverse(root);
@@ -53,5 +50,37 @@ public class No543_DiameterOfBinaryTree {
         int temp = left + right;
         if (max < temp) max = temp;
         return left > right ? left + 1 : right + 1;  // root 的深度
+    }
+
+
+    int diameter4 = 0;
+    public int diameterOfBinaryTree4(TreeNode root) {
+        if (root == null) return 0;
+        depth4(root);
+        return diameter4;
+    }
+
+    private int depth4(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = depth4(root.left);
+        int right = depth4(root.right);
+
+        // 更新最大直径：节点之间的路径长度 = 左子树深度 + 右子树深度
+        diameter4 = Math.max(diameter4, left + right);
+        // 返回节点为根的子树的最大深度
+        return Math.max(left, right) + 1;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        No543_DiameterOfBinaryTree solution = new No543_DiameterOfBinaryTree();
+        System.out.println("The diameter of the binary tree is: " + solution.diameterOfBinaryTree4(root));
     }
 }
