494656ff3a11cbc23884d0e4dc8251b96e3b3fe5
Tsing12Travel <Tsing12Travel@163.com>
2024-08-02 19:03:15 +0800

    No437_PathSum

--- src/main/java/top100/No437_PathSum.java
+++ src/main/java/top100/No437_PathSum.java
@@ -7,7 +7,7 @@ public class No437_PathSum {
     public int pathSum(TreeNode root, int targetSum) {
         Map<Long, Integer> cnt = new HashMap<>();
         cnt.put(0L, 1);
-        return dfs(root, 0,targetSum,cnt);
+        return dfs(root, 0, targetSum, cnt);
     }
 
     public int dfs(TreeNode node, long s, int targetSum, Map<Long, Integer> cnt) {
@@ -41,5 +41,37 @@ public class No437_PathSum {
         res += rootSum(root.left, targetSum - val);
         res += rootSum(root.right, targetSum - val);
         return res;
+    }
+
+
+    public int pathSum3(TreeNode root, int targetSum) {
+        // HashMap to store (prefix_sum, count) pairs
+        Map<Integer, Integer> prefixSums = new HashMap<>();
+        prefixSums.put(0, 1);
+        return dfs(root, 0, targetSum, prefixSums);
+    }
+
+    private int dfs(TreeNode node, int currentSum, int targetSum, Map<Integer, Integer> prefixSums) {
+        if (node == null) {
+            return 0;
+        }
+
+        // Update current sum
+        currentSum += node.val;
+
+        // Get the number of valid paths that end at this node
+        int numPathsToCurr = prefixSums.getOrDefault(currentSum - targetSum, 0);
+
+        // Update the prefixSums map
+        prefixSums.put(currentSum, prefixSums.getOrDefault(currentSum, 0) + 1);
+
+        // Recurse to left and right subtrees
+        numPathsToCurr += dfs(node.left, currentSum, targetSum, prefixSums);
+        numPathsToCurr += dfs(node.right, currentSum, targetSum, prefixSums);
+
+        // Remove the current sum from the map to backtrack
+        prefixSums.put(currentSum, prefixSums.get(currentSum) - 1);
+
+        return numPathsToCurr;
     }
 }
