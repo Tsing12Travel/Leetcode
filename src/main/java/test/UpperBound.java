450ceeedec6bca2f3163cd2d8d2332d4590eddc7
Tsing12Travel <Tsing12Travel@163.com>
2024-08-06 20:54:01 +0800

    UpperBound

--- src/main/java/test/UpperBound.java
+++ src/main/java/test/UpperBound.java
@@ -1,21 +1,25 @@
 package test;
 
 public class UpperBound {
-    public int upperBound(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return 0;
-
+    public int upperBound(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        int result = -1;  // 初始化结果为 -1，表示未找到目标值
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] <= k) {  // nums[mid] = k 时，左边界依然右移，直到越界，即 left > right，故等于 k 的数的右边界为 right
-                left = mid + 1;
+
+            if (nums[mid] == target) {
+                result = mid;  // 记录当前找到的目标值下标
+                left = mid + 1;  // 继续搜索右侧部分，寻找最后一个出现的目标值
+            } else if (nums[mid] < target) {
+                left = mid + 1;  // 搜索右半部分
             } else {
-                right = mid - 1;
+                right = mid - 1;  // 搜索左半部分
             }
         }
-        return right;  // 注意返回值
+
+        return result; // 返回找到的最后一个目标值的下标
     }
 
 
