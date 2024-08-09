837984d02988df1cbd2f3b2c3d1acf0744e1d6cb
Tsing12Travel <Tsing12Travel@163.com>
2024-08-09 19:21:31 +0800

    No207_CanFinish

--- src/main/java/top100/No207_CanFinish.java
+++ src/main/java/top100/No207_CanFinish.java
@@ -79,7 +79,7 @@ public class No207_CanFinish {
 
 
     public boolean canFinish2(int numCourses, int[][] prerequisites) {
-        int[] indegrees = new int[numCourses];  // 创建一个数组，用于存放每门课程的入度（即先修课程的数量）
+        int[] inDegree = new int[numCourses];  // 创建一个数组，用于存放每门课程的入度（即先修课程的数量）-> 入度数组 inDegree：inDegree[i] 表示课程 i 需要先修的课程数量
         List<List<Integer>> adjacency = new ArrayList<>();  // 创建一个邻接表，用于存放每门课程的后续课程
         Queue<Integer> queue = new LinkedList<>();  // 创建一个队列，用于存放入度为 0 的课程（即没有先修课程，可以直接学习的课程）
 
@@ -90,13 +90,13 @@ public class No207_CanFinish {
 
         // 获取每门课程的入度和邻接表
         for (int[] cp : prerequisites) {
-            indegrees[cp[0]]++;  // cp[0] 的入度加一，想要学习 cp[0] 就得先完成 cp[1] 即 cp[0] <- cp[1]
+            inDegree[cp[0]]++;  // cp[0] 的入度加一，想要学习 cp[0] 就得先完成 cp[1] 即 cp[0] <- cp[1]
             adjacency.get(cp[1]).add(cp[0]);  // cp[0] 是 cp[1] 的后续课程，所以将 cp[0] 加入 cp[1]
         }
 
         // 获取所有入度为 0 的课程
         for (int i = 0; i < numCourses; i++) {
-            if (indegrees[i] == 0) {  // 如果一门课程的入度为 0，那么将其加入队列
+            if (inDegree[i] == 0) {  // 如果一门课程的入度为 0，那么将其加入队列
                 queue.add(i);
             }
         }
@@ -107,12 +107,12 @@ public class No207_CanFinish {
             numCourses--;  // 学完一个课程，待学习的课程数减 1
 
             for (int cur : adjacency.get(pre)) {  // 学完该课程，需要把其邻接课程的入度减 1
-                /*if (--indegrees[cur] == 0) {  // 如果一门后续课程的入度减为 0，那么将其加入队列
+                /*if (--inDegree[cur] == 0) {  // 如果一门后续课程的入度减为 0，那么将其加入队列
                     queue.add(cur);
                 }*/
-                indegrees[cur]--;
+                inDegree[cur]--;
                 // 邻接课程 cur 的入度更新后，需要判断是否为 0，若为 0，证明课程 cur 也可以学习了，需要加到可学课程队列中
-                if (indegrees[cur] == 0) {
+                if (inDegree[cur] == 0) {
                     queue.add(cur);
                 }
             }
