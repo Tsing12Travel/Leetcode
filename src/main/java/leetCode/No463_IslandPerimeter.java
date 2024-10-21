fe725b510470dd0faba89ed350c25cb8c32a5b3d
Tsing12Travel <Tsing12Travel@163.com>
2024-10-21 12:12:48 +0800

    No463_IslandPerimeter

--- src/main/java/leetCode/No463_IslandPerimeter.java
+++ src/main/java/leetCode/No463_IslandPerimeter.java
@@ -38,9 +38,9 @@ public class No463_IslandPerimeter {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    if (j - 1 < 0 || (j - 1 >= 0 && grid[i][j - 1] == 0)) count++;
+                    if (j - 1 < 0 || grid[i][j - 1] == 0) count++;
                     if (j + 1 >= grid[0].length || (j + 1 < grid[0].length && grid[i][j + 1] == 0)) count++;
-                    if (i - 1 < 0 || (i - 1 >= 0 && grid[i - 1][j] == 0)) count++;
+                    if (i - 1 < 0 || grid[i - 1][j] == 0) count++;
                     if (i + 1 >= grid.length || (i + 1 < grid.length && grid[i + 1][j] == 0)) count++;
                 }
             }
