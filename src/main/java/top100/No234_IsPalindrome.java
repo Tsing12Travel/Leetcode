41905e99cb5da809536350c0dda753ab39734a0a
Tsing12Travel <Tsing12Travel@163.com>
2024-11-11 13:10:50 +0800

    No234_IsPalindrome

--- src/main/java/top100/No234_IsPalindrome.java
+++ src/main/java/top100/No234_IsPalindrome.java
@@ -24,4 +24,59 @@ public class No234_IsPalindrome {
 
         return true;
     }
+
+
+    public boolean isPalindrome2(ListNode head) {
+        ListNode mid = findMid(head);
+        ListNode midHead = reverse(mid);
+
+        while (midHead != null) {
+            if (midHead.val != head.val) return false;  // 不是回文链表
+
+            midHead = midHead.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    // 876.链表中间节点
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // 206.反转链表(此处使用迭代方式翻转，而不能使用递归，否则空间复杂度将变成 O(N)
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(2);
+
+        No234_IsPalindrome No234 = new No234_IsPalindrome();
+        System.out.println(No234.isPalindrome2(head));
+    }
 }
