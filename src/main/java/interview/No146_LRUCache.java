33573ca5af0eebf133f6aed37d5a32bb32c0843e
Tsing12Travel <Tsing12Travel@163.com>
2025-03-06 22:11:10 +0800

    No146_LRUCache

---
+++ src/main/java/interview/No146_LRUCache.java
@@ -0,0 +1,34 @@
+package interview;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// 2025.03.06 ZOOM 一面笔试
+class No146_LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+
+    public No146_LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
