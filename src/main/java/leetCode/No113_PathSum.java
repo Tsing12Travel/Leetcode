88bfdf0eb3bcaf1bd93b640afa0cac71376efbc2
Tsing12Travel <Tsing12Travel@163.com>
2025-03-23 09:56:01 +0800

    No113_PathSum

---
+++ src/main/java/leetCode/No113_PathSum.java
@@ -0,0 +1,52 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No113_PathSum {
+    List<List<Integer>> res;
+    List<Integer> path;
+    int targetSum;
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        res = new ArrayList<>();
+        path = new ArrayList<>();
+        this.targetSum = targetSum;
+
+        dfs(0, root);
+        return res;
+    }
+
+    private void dfs(int sum, TreeNode node) {
+        if (node == null) return;
+
+        sum += node.val;
+        path.add(node.val);
+        if (node.left == node.right) {  // 叶节点
+            if (sum == targetSum) {
+                res.add(new ArrayList<>(path));
+            }
+        } else {
+            dfs(sum, node.left);
+            dfs(sum, node.right);
+        }
+        path.removeLast();
+    }
+
+
+    public static void main(String[] args) {
+        No113_PathSum No113 = new No113_PathSum();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(1);
+        List<List<Integer>> res = No113.pathSum(root, 22);
+        System.out.println(res);
+    }
+}
